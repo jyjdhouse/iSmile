93c9e540c41eadd324c9d659f0926ef95c10b7d3
Jano <janoo.pereira@gmail.com>
2023-06-05 11:32:24 -0300

    SlideShow animation

--- public/scripts/indexScript.js
+++ public/scripts/indexScript.js
@@ -69,34 +69,63 @@ window.addEventListener('load', () => {
             return
         }
     }
-    const handleVisibleLabel = (label) =>{
+    const handleVisibleLabel = (label) => {
         label.classList.add('slide-image-label-visible');
     }
     // Crear una instancia del Intersection Observer
-    const observer = checkIfIsInScreen(.4,handleAboutUsContainer,true)
+    const observer = checkIfIsInScreen(.4, handleAboutUsContainer, true)
 
     // Logica para mostrar labels en slideShow
     const labels = document.querySelectorAll('.slide-image-label');
-    const slideImagesContainers = document.querySelectorAll('.slide-image-container')
-    if(isInDesktop()){ //Si esta en desktop es distinto el slideShow
-        slideImagesContainers.forEach(container => {
-            container.addEventListener('mouseenter',()=>{
+    const slideImageContainers = document.querySelectorAll('.slide-image-container')
+    if (isInDesktop()) { //Si esta en desktop es distinto el slideShow
+        slideImageContainers.forEach(container => {
+            container.addEventListener('mouseenter', () => {
                 const labelToActivate = container.querySelector('.slide-image-label');
-                labelToActivate.classList.add('slide-image-label-visible')
+                labelToActivate.classList.add('slide-image-label-visible');
+                // Freno el slideShow
+                clearInterval(slideIntervalId);
             });
-            container.addEventListener('mouseleave',()=>{
+            container.addEventListener('mouseleave', () => {
                 const labelToDeactivate = container.querySelector('.slide-image-label');
-                labelToDeactivate.classList.remove('slide-image-label-visible')
+                labelToDeactivate.classList.remove('slide-image-label-visible');
+                slideIntervalId = setInterval(updateSlideShow, 5000);
             });
         });
-    } else{ //Aca es otra logica
-        labels.forEach(lab=>{
-            const labelObserver = checkIfIsInScreen(.5,handleVisibleLabel,lab);
+    } else { //Aca es otra logica
+        labels.forEach(lab => {
+            const labelObserver = checkIfIsInScreen(.5, handleVisibleLabel, lab);
             labelObserver.observe(lab);
         });
     }
-    
+
     // Observar el contenedor
     observer.observe(aboutUsContainer);
+
+    // LOGICA para slideShow
+    let slideImagesGroup;
+    // Depende que resolucion agarro los diferentes grupos
+    if(isInDesktop()){
+        slideImagesGroup = document.querySelector('.slide-images-wraper-desktop').querySelectorAll('.slide-image-group')
+    } else{
+        slideImagesGroup = document.querySelector('.slide-images-wraper-mobile').querySelectorAll('.slide-image-group')
+    }
+    let currentIndex = 1;
+
+    function updateSlideShow() {
+        slideImagesGroup.forEach((container, index) => {
+          container.classList.remove('slide-image-group-active', 'slide-image-group-next', 'slide-image-group-prev');
+          if (index === currentIndex) {
+            container.classList.add('slide-image-group-active');
+          } else if (index === (currentIndex + 1) % slideImagesGroup.length) {
+            container.classList.add('slide-image-group-next');
+          } else if (index === (currentIndex - 1 + slideImagesGroup.length) % slideImagesGroup.length) {
+            container.classList.add('slide-image-group-prev');
+          }
+        });
+        currentIndex = (currentIndex + 1) % slideImagesGroup.length;
+      }
+    let slideIntervalId = setInterval(updateSlideShow, 5000);
+
 
 })
