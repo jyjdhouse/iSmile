b3f68079e8a56c67a5c518eee652b037db1b0bdb
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-06-09 16:29:37 -0300

    footer

--- public/scripts/footerScript.js
+++ public/scripts/footerScript.js
@@ -3,45 +3,75 @@ window.addEventListener('load', () => {
     const footerIconsContainer = document.querySelectorAll('.footer-icon-container')
     const footerDataIconsContainer = document.querySelectorAll('.mobile-footer-data-icon-container')
     const dataXContainer = document.querySelectorAll('.x-container')
-    const phoneNum = '1124852020'
-    
+    const closeFooterDataBtn = document.querySelectorAll('.close-footer-data')
+    const addressBtn = document.querySelector('.open-map');
+    const copyAddress = document.querySelector('.copy-address');
+    const copyNumber = document.querySelector('.copy-number');
+    const callNumber = document.querySelector('.call-number');
+
     // logica para clickear iconos en footer mobile
     footerIconsContainer.forEach((icon, indexIcon) => {
         icon.addEventListener('click', () => {
             // como estan separados los iconos de la data de cada uno
             // pregunto si el index del icon es igual al index de la data
             // y agrego clase
-            for(let indexData = 0; indexData < footerDataIconsContainer.length; indexData++) {
-                if(indexIcon === indexData && indexData != 1){  
+            for (let indexData = 0; indexData < footerDataIconsContainer.length; indexData++) {
+                if (indexIcon === indexData) {
                     footerDataIconsContainer[indexData].classList.add('mobile-footer-data-icon-container-active')
-                } else {
-                    window.location.href = 'tel:' + phoneNum
                 }
             }
 
         })
     });
-    
+
     dataXContainer.forEach((item, indexItem) => {
         item.addEventListener('click', () => {
-            for(let indexData = 0; indexData < footerDataIconsContainer.length; indexData++) {
-                if(indexItem === indexData){  
+            for (let indexData = 0; indexData < footerDataIconsContainer.length; indexData++) {
+                if (indexItem === indexData) {
                     footerDataIconsContainer[indexData].classList.remove('mobile-footer-data-icon-container-active')
                 }
             }
         })
     })
 
-      //Para llamar cuando toca
-      const callBtn = document.querySelector('.call-btn');
-      callBtn.addEventListener('click',()=>{
-          window.location.href = "tel:+541159442644";
-      });
 
-      //Para abrir maps
-      const addressBtn = document.querySelector('.address-btn');
-      addressBtn.addEventListener('click',()=>{
-        window.open('https://goo.gl/maps/tt2tMzZsEzj7UYxa9','_blank');
+    closeFooterDataBtn.forEach((btn, indexBtn) => {
+        btn.addEventListener('click', () => {
+            for (let indexFooterData = 0; indexFooterData < footerDataIconsContainer.length; indexFooterData++) {
+                if (indexBtn == indexFooterData) {
+                    footerDataIconsContainer[indexBtn].classList.remove('mobile-footer-data-icon-container-active')
+                }
+            }
+        })
+    })
+
+    //Para llamar cuando toca
+    const phoneNumber = '+541159442644'
+    callNumber.addEventListener('click', () => {
+        window.location.href = `tel:${phoneNumber}`;
+    });
+
+    copyNumber.addEventListener('click', () => {
+        navigator.clipboard.writeText(phoneNumber)
+        copyNumber.innerHTML = 'Número copiado'
+        setTimeout(() => {
+            copyNumber.innerHTML = 'Copiar número de teléfono en portapapeles'
+        }, 2000)
+    })
+
+    // Para copiar al portapapeles
+    const address = 'Avenida Santa Fé 2911 3 F, C1425 Buenos Aires'
+    copyAddress.addEventListener('click', () => {
+        navigator.clipboard.writeText(address)
+        copyAddress.innerHTML = 'Dirección copiada'
+        setTimeout(() => {
+            copyAddress.innerHTML = 'Copiar dirección en el portapapeles'
+        }, 2000)
+    })
+
+    //Para abrir maps
+    addressBtn.addEventListener('click', () => {
+        window.open('https://goo.gl/maps/tt2tMzZsEzj7UYxa9', '_blank');
     });
 
 })
