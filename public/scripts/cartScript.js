0f531d29cc17e9b1ceab441961824611b08141d9
Jano <janoo.pereira@gmail.com>
2023-06-13 18:54:01 -0300

    Blog list - Index css details - Removed cart

--- public/scripts/cartScript.js
+++ public/scripts/cartScript.js
@@ -1,21 +1,5 @@
 window.addEventListener('load', () => {
-    // LOGICA DEL POPUP CART
-
-    const openCartPopupBtn = document.querySelector('.open-popup-cart-btn');
-    const cartPopup = document.querySelector('.cart-popup-container');
-    const blackScreen = document.querySelector('.black-screen');
-    const closePopupCartBtn = document.querySelector('.close-popup-cart-btn');
-    openCartPopupBtn?.addEventListener('click', () => {
-        cartPopup.classList.add('cart-popup-container-active');
-        blackScreen.classList.add('black-screen-active');
-    });
-
-    closePopupCartBtn?.addEventListener('click', () => {
-        cartPopup.classList.remove('cart-popup-container-active');
-        blackScreen.classList.remove('black-screen-active');
-    });
-    blackScreen?.addEventListener('click', () => {
-        cartPopup.classList.remove('cart-popup-container-active');
-        blackScreen.classList.remove('black-screen-active');
-    });
+    // Apenas arranca le saco la clase hidden
+    const shopingCartQuickPopup = document.querySelector('.add-to-cart-container');
+    shopingCartQuickPopup.classList.remove('hidden')
 });
