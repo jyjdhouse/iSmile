ca1463766e2934284a245dd5f8a7eca80421d7ed
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-07-05 09:36:36 -0300

    script

--- public/scripts/createBlog.js
+++ public/scripts/createBlog.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
     const textArea = form.querySelector('textarea')
     let formErrors = false
     let images = document.querySelector('.images')
+    let imagesRadioContentContainer = document.querySelector('.images-radio-content-container')
 
 
     form.addEventListener('submit', (e) => {
@@ -13,24 +14,41 @@ window.addEventListener('load', () => {
         formErrors = false
         inputs.forEach(inp => {
             inp.classList.remove('input-error')
-            if(!inp.value){
+            if (!inp.value) {
                 inp.classList.add('input-error')
                 formErrors = true
             }
-           
+
         })
 
         textArea.classList.remove('input-error')
-        if(textArea.value.trim() == ''){
-            
+        if (textArea.value.trim() == '') {
+
             textArea.classList.add('input-error')
             formErrors = true
         }
 
-        if(!formErrors){
+        if (!formErrors) {
             form.submit()
         }
 
     })
+
+    images.addEventListener('change', () => {
+
+        for (var i = 0; i < images.length; i++) {
+            let divContainer = document.createElement('div');
+            divContainer.classList.add('radio-input-container');
+
+            imagesRadioContentContainer.innerHTML += divContainer
+
+
+            
+
+        }
+
+
+    })
+
 
 })
