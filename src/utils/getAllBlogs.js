874c5ca439a65573409a9200978c1ef00437cfe4
Jano <janoo.pereira@gmail.com>
2023-08-07 18:29:08 -0300

    Modified getAllblogs

--- src/utils/getAllBlogs.js
+++ src/utils/getAllBlogs.js
@@ -1,6 +1,7 @@
 const db = require('../database/models');
 module.exports = async function (id) {
     return await db.Blog.findAll({
+        order: [['createdAt', 'DESC']],
         include: [
             'files',       
         ]
