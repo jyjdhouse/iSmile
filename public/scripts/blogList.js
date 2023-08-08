53c4a9f421ae3c05dd732862898e4d2ff6590a7c
Jano <janoo.pereira@gmail.com>
2023-08-08 13:34:55 -0300

    Added prettyDate in blogDetail

--- public/scripts/blogList.js
+++ public/scripts/blogList.js
@@ -1,7 +1,8 @@
 import { getPrettyDate, getPrettyDateReversed } from './utils.js'
 window.addEventListener('load', () => {
     // Logica para agarrar todas las cards y pintarle bien la fecha
-    const blogCards = document.querySelectorAll('.blog-link');
+    let blogCards = document.querySelectorAll('.blog-link'); 
+    !blogCards.length ? blogCards = document.querySelectorAll('.last-blog-card'): null;
     blogCards.forEach(card => {
         const date = card.querySelector('.blog-date');
         const prettyDate = getPrettyDateReversed(date.innerText);
