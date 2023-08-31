616f52c8d4c4e54fabeff86cd08ff93eb18287b1
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-08-31 18:06:26 -0300

    consoleloguing erros

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -241,6 +241,7 @@ const controller = {
             let userBodyData = req.body;
             if (!errors.isEmpty()) { //Si hay errores en el back...
                 errors = errors.mapped();
+                console.log(errors)
                 // return res.send(errors)
                 return res.redirect('/user/profile?putErrors=true');
             }
